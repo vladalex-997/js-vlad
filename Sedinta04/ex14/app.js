a95f69b7f945dc5243c89e4c6ff6dc52716e14da
Unknown <it@nerv.ro>
2019-03-18 16:42:54 +0200

    exercises for course 4

---
+++ Sedinta04/ex14/app.js
@@ -0,0 +1,22 @@
+var person = {
+  firstName: 'Dragos',
+  lastName: 'Iordache',
+  birthYear: 1987, 
+  pets: [{
+    name: 'Twix',
+    species: 'papagal',
+    age: 4
+  }, {
+    name: 'Mars',
+    species: 'caine',
+    age: 2, 
+  }, {
+    name: 'Bounty',
+    species: 'hamster',
+    age: 4
+  }, {
+    name: 'Lion',
+    species: 'pisica',
+    age: 10
+  }]
+};
