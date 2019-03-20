bf09c0b0ae56103aea4bbe573f435dbfc8d4de9e
Unknown <it@nerv.ro>
2019-03-20 15:17:07 +0200

    exercises for OOP

---
+++ Sedinta07/ex05/app.js
@@ -0,0 +1,72 @@
+class Shape {
+  constructor(
+    posX,
+    posY, 
+    width, 
+    height, 
+    color, 
+    borderColor
+  ) {
+    this.posX = posX; 
+    this.posY = posY; 
+    this.height = height; 
+    this.width = width; 
+    this.color = color; 
+    this.borderColor = borderColor;
+  }
+
+  
+  setPosX(x) {
+    this.posX = x;
+  }
+
+  setPosY(y) {
+    this.posY = y;
+  }
+
+  setWidth(w) {
+    this.width = w;
+  }
+
+  setHeight(h) {
+    this.height = h;
+  }
+
+  setColor(c) {
+    this.color = c; 
+  }
+
+  setBorderColor(bc) {
+    this.borderColor = bc;
+  }
+}
+
+class Rectangle extends Shape {
+  
+}
+
+class Circle extends Shape {
+  constructor(
+    posX,
+    posY, 
+    radius, 
+    color, 
+    borderColor
+  ) {
+    super(
+      posX,
+      posY, 
+      radius, 
+      radius, 
+      color, 
+      borderColor
+    );
+
+    this.borderRadius = this.setBorderRadius(radius);
+  }
+
+  setBorderRadius(br) {
+
+    return br;
+  }
+}
