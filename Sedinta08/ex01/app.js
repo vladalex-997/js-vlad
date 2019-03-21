cea58e15a5291bc36709e5656ea460bfd4a154fb
Unknown <it@nerv.ro>
2019-03-21 16:44:14 +0200

    exercises for 08

---
+++ Sedinta08/ex01/app.js
@@ -0,0 +1,6 @@
+// var p = document.createElement('p');
+// p.innerText = 'Textul care trebuie sa apara in paragraf';
+// p.id = 'myParagraph';
+// p.classList.add('clase', 'de', 'css');
+
+// document.body.appendChild(p);
