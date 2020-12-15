be0ab388fcd3fd67a0f29b8b9ea9817b51f460bc
ianiri <44301798+ianiri@users.noreply.github.com>
2020-12-15 22:06:37 +0200

    done

---
+++ M01S03/ex07/app.js
@@ -0,0 +1,6 @@
+console.log(`
+    Dragos este instructor de programare. Dragos studiaza JavaScript de la 17 ani. 
+    Cand Dragos a absolvit facultatea si-a facut licenta in JavaScript. 
+    Dragos are si un master in tehnologii internet. 
+    Dragos este instructor Pixellab si preda JavaScript acolo.
+`);
