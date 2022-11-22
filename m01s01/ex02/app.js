af577224c6d74ef4a33d2d32c05f8864efc5368d
Dragos Iordache <dragosh.iordache@yahoo.com>
2022-11-22 20:38:41 +0200

    838

--- m01s01/ex02/app.js
+++ m01s01/ex02/app.js
@@ -1,1 +1,20 @@
+var inputLength = document.getElementById('length');
+var inputWidth = document.getElementById('width');
+var elementResult = document.getElementById('result');
+var form = document.querySelector('form');
 
+form.addEventListener(
+  'submit',
+  function (event) {
+    // event.prevent default obligatoriu
+    event.preventDefault();
+    var length = inputLength.value || 0;
+    var width = inputWidth.value || 0;
+    var result = 0;
+
+    result = length * width;
+
+    elementResult.innerText = result;
+  },
+  false,
+);
