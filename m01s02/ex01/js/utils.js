8455c63b3144e355d4e7ef8bb468752e7500a62a
Dragos Iordache <dragosh.iordache@yahoo.com>
2022-11-22 21:59:57 +0200

    first js

---
+++ m01s02/ex01/js/utils.js
@@ -0,0 +1,12 @@
+console.log((45 * 82).toString());
+console.warn(`
+  Din fisierul utils.js afiseaza in consola
+  patratul sumei numerelor 13 si 41. a2  + 2ab + b2
+`);
+console.log((13 * 13 + 2 * 13 * 41 + 41 * 41).toString());
+console.warn(`
+  Din fisierul utils.js afiseaza in consola
+  suprafata unui cerc cu diametrul de 15
+  (foloseste obiectul Math)
+`);
+console.log((Math.pow(15 / 2, 2) * Math.PI).toString());
