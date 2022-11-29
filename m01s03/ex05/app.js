4d406ca0cd7aae3dfdc9c0aee9c5b9690ac203c7
Vlad Alex <vladalex997@gmail.com>
2022-11-29 21:58:23 +0200

    curs 29/11

---
+++ m01s03/ex05/app.js
@@ -0,0 +1,63 @@
+var person = {
+  name: 'Gigel',
+  surname: 'Marcel',
+  age: 15,
+  petOwner: false,
+  skills: ['html', 'javascript', 'css'],
+  friends: [
+    {
+      name: 'Larry',
+      surname: 'Larryson',
+      age: 30,
+    },
+    {
+      name: 'Steven',
+      surname: 'Stevenson',
+      age: 31,
+    },
+    {
+      name: 'Carol',
+      surname: 'Carolson',
+      age: 29,
+    },
+  ],
+};
+
+console.warn(
+  'Afiseaza propozitia: “Ma numesc xxx yyy si stiu html si css.” Foloseste notatia cu paranteze patrate',
+);
+
+console.log(
+  'Ma numesc ' +
+    person.name +
+    ' ' +
+    person.surname +
+    ' si stiu ' +
+    person.skills[0] +
+    ' si ' +
+    person.skills[2] +
+    '.',
+);
+
+console.warn(`Afiseaza propozitia “Sunt xxx si acum invat JavaScript.
+” Nu folosi valoarea din arrayul de skills.
+`);
+
+console.log('Sunt ' + person.name + ' si acum invat JavaScript.');
+
+console.warn(
+  `Afiseaza propozitia “Am x prieteni: Larry, Steven si Carol.”
+   Foloseste proprietatea length si apeleaza direct proprietatile name ale obiectelor.`,
+);
+
+console.log(
+  'Am ' +
+    person.friends.length +
+    ' prieteni: ' +
+    person.friends[0].name +
+    ', ' +
+    person.friends[1].name +
+    ' si ' +
+    person.friends[2].name +
+    '.',
+);
