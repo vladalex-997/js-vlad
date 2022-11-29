7757c01a3248e558b66db000163455a84c744ca9
Dragos Iordache <dragosh.iordache@yahoo.com>
2022-11-29 20:28:51 +0200

    828

---
+++ m01s03/ex04/app.js
@@ -0,0 +1,26 @@
+var myArray = [1, 2, 3, 4, 5, 6];
+
+console.warn(`
+  Afiseaza numarul de pe pozitia 5 a arrayului.
+`);
+console.log(myArray[5].toString());
+
+console.warn(`
+  Afiseaza numarul 3 din array.
+`);
+console.log(myArray[2].toString());
+
+console.warn(`
+  Afiseaza lungimea arrayului.
+`);
+console.log(myArray.length);
+
+console.warn(`
+  Afiseaza suma numerelor de pe pozitia 0 si 4.
+`);
+console.log(myArray[0] + myArray[4]);
+
+console.warn(`
+  Afiseaza pozitia 1.
+`);
+console.log(myArray[1].toString());
