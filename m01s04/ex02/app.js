741dc1ce31f187b587328b49fb6e9099d11bfb93
Dragos Iordache <dragosh.iordache@yahoo.com>
2022-12-01 21:55:44 +0200

    955

---
+++ m01s04/ex02/app.js
@@ -0,0 +1,19 @@
+var errorCode = 600;
+
+if (errorCode === 0) {
+  console.log('No error');
+} else if (errorCode === 100) {
+  console.log('Error 100');
+} else if (errorCode === 200) {
+  console.log('Error 200');
+} else if (errorCode === 300) {
+  console.log('Error 300');
+} else if (errorCode === 400) {
+  console.log('Error 400');
+} else if (errorCode === 500) {
+  console.log('Error 500');
+} else if (errorCode === 600) {
+  console.log('Error 600');
+} else {
+  console.log('Something else');
+}
