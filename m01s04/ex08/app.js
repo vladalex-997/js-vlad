d5ec625249cb4d02b25c3e460ecc4ebb7e337abd
Dragos Iordache <dragosh.iordache@yahoo.com>
2022-12-06 21:53:47 +0200

    953

---
+++ m01s04/ex08/app.js
@@ -0,0 +1,23 @@
+var i = 1;
+
+// while (i <= 100) {
+//   console.log(i);
+
+//   i++;
+// }
+
+// while (i < 100) {
+//   console.log(i);
+
+//   i++;
+// }
+
+while (i < 100) {
+  i++;
+
+  console.log(i);
+
+  if (i === 50) {
+    break;
+  }
+}
