e9a553265d508ad3279872298005a507f1becca6
Dragos Iordache <dragosh.iordache@yahoo.com>
2022-12-15 20:31:42 +0200

    831

---
+++ m01s05/ex06/app.js
@@ -0,0 +1,15 @@
+const person = {
+  getName: function () {
+    return 'Dragos Iordache';
+  },
+
+  getAge: function () {
+    return 32;
+  },
+};
+
+function accessor(methodSuffix) {
+  const methodName = `get${methodSuffix}`;
+
+  return person[methodName]();
+}
