c54e6478aa34edb0c96146fb1f17a8a9445b85ee
Dragos Iordache <dragosh.iordache@yahoo.com>
2023-01-17 18:21:20 +0000

    621

--- m01s06/ex04/app.js
+++ m01s06/ex04/app.js
@@ -1,5 +1,28 @@
-(function() {
-  document.addEventListener('DOMContentLoaded', function() {
-      
+(function () {
+  document.addEventListener('DOMContentLoaded', function () {
+    const stage = document.querySelector('.stage');
+
+    stage.addEventListener('mouseover', function () {
+      const message = 'Mouseul este pe scena';
+
+      showMessage(message, 'message');
+      console.log(message);
+    });
+
+    stage.addEventListener('mouseout', function () {
+      const message = 'Mouseul nu este pe scena';
+
+      showMessage(message, 'message');
+      console.log(message);
+    });
+
+    // function functions are hoisted
+    function showMessage(message, cssClass = '') {
+      const paragraphElement = document.createElement('p');
+      paragraphElement.innerText = message;
+      paragraphElement.classList.add(cssClass);
+
+      document.body.append(paragraphElement);
+    }
   });
 })();
