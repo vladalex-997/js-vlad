577572106d563c0e2d8eb35a156a4fd4d429832d
Dragos Iordache <dragosh.iordache@yahoo.com>
2023-01-24 19:20:56 +0200

    720

--- m01s06/ex04/app.js
+++ m01s06/ex04/app.js
@@ -1,5 +1,26 @@
-(function() {
-  document.addEventListener('DOMContentLoaded', function() {
-      
+(function () {
+  document.addEventListener('DOMContentLoaded', function () {
+    const stage = document.querySelector('.stage');
+
+    stage.addEventListener('mouseover', () => {
+      const message = 'Mouseul este pe scena';
+      console.log(message);
+      showMessage(message);
+    });
+
+    stage.addEventListener('mouseout', () => {
+      const message = 'Mouseul nu este pe scena';
+      console.log(message);
+      showMessage(message);
+    });
+
+    // function functions are hoisted
+    function showMessage(message) {
+      const paragraphElement = document.createElement('p');
+      paragraphElement.innerText = message;
+      paragraphElement.classList.add('message');
+
+      document.body.append(paragraphElement);
+    }
   });
 })();
--- m01s06/ex05/app.js
+++ m01s06/ex05/app.js
@@ -1,5 +1,61 @@
-(function() {
-  document.addEventListener('DOMContentLoaded', function() {
-    
+(function () {
+  document.addEventListener('DOMContentLoaded', function () {
+    const showButton = document.querySelector('#showButton');
+    const toggleEventButton = document.querySelector('#toggleEventButton');
+    const removeButton = document.querySelector('#removeButton');
+    let eventBound = true;
+
+    showButton.addEventListener('click', showAlert);
+    showMessage('Alerta va fi afisata');
+
+    removeButton.addEventListener('click', function () {
+      // early return
+      if (!confirm('Stergem butoanele?')) {
+        return;
+      }
+
+      toggleEventButton.remove();
+      showButton.remove();
+    });
+
+    toggleEventButton.addEventListener('click', function () {
+      let message = '';
+      let label = '';
+
+      if (eventBound) {
+        // if event is bound, remove
+        showButton.removeEventListener('click', showAlert);
+        eventBound = false;
+        message = 'Porneste afisarea';
+        label = 'Alerta NU va fi afisata';
+      } else {
+        // if event is not bound, add
+        showButton.addEventListener('click', showAlert);
+        eventBound = true;
+        message = 'Opreste afisarea';
+        label = 'Alerta va fi afisata';
+      }
+
+      this.innerText = message;
+      showMessage(label);
+    });
+
+    // function functions are hoisted
+    function showAlert() {
+      alert('Butonul a fost pasat');
+    }
+
+    function showMessage(message) {
+      let paragraphElement = document.querySelector('.message');
+
+      if (paragraphElement === null) {
+        paragraphElement = document.createElement('p');
+        paragraphElement.classList.add('message');
+      }
+
+      paragraphElement.innerText = message;
+
+      document.body.append(paragraphElement);
+    }
   });
 })();
